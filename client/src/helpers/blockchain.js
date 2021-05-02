21fa9d37f52a054fdabedb2afecd26c4edd3f2b3
DevTiqo <lilfreshy33@yahoo.com>
2021-05-02 13:17:43 +0100

    Bug fixes

--- client/src/helpers/blockchain.js
+++ client/src/helpers/blockchain.js
@@ -6,9 +6,9 @@ import Web3 from 'web3';
 export const BlockChain = {
     loadCandidates,
     loadWeb3,
-loadBlockchainData,
-addCand,
-vote,
+    loadBlockchainData,
+    addCand,
+    vote,
     // adminLogin,
     // candLogin,
     // logout,
@@ -17,100 +17,100 @@ vote,
 };
 
 
-  loadWeb3();
+loadWeb3();
 loadBlockchainData();
 
 
 var election;
 var account;
-async function loadBlockchainData(){
-        var web3 = await window.web3
-        var accounts = await web3.eth.getAccounts()
-        
-        account= accounts[0];
-        var networkId = await web3.eth.net.getId()
+async function loadBlockchainData() {
+    var web3 = await window.web3
+    var accounts = await web3.eth.getAccounts()
 
-          console.log(networkId)
-        console.log(accounts)
-        const networkData = Election.networks[networkId]
-        election=""
-        if(networkData) {
-               let contract_address = "0x60888809Ab8C90651666FDF2664D3666991Cd296"; 
+    account = accounts[0];
+    var networkId = await web3.eth.net.getId()
 
-             election = new web3.eth.Contract(Election.abi, networkData.address)
-           
-            console.log(election);
+    console.log(networkId)
+    console.log(accounts)
+    const networkData = Election.networks[networkId]
+    election = ""
+    if (networkData) {
+        let contract_address = "0x60888809Ab8C90651666FDF2664D3666991Cd296";
 
-          } else {
-            window.alert('Election contract not deployed to detected network.')
-        }
-        
+        election = new web3.eth.Contract(Election.abi, networkData.address)
+
+        console.log(election);
+
+    } else {
+        window.alert('Election contract not deployed to detected network.')
+    }
+
 }
-        
-      
 
-    async function loadWeb3() {
-        if (Web3.givenProvider || "ws://localhost:8545") {
-            window.web3 = new Web3(Web3.givenProvider || "ws://localhost:8545")
-            
-        } 
-        else if (window.ethereum) {
-            window.web3 = new Web3(window.ethereum)
-            await window.ethereum.enable()
-        }
-        else {
-            window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+
+
+async function loadWeb3() {
+    if (Web3.givenProvider || "ws://localhost:8545") {
+        window.web3 = new Web3(Web3.givenProvider || "ws://localhost:8545")
+
+    }
+    else if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum)
+        await window.ethereum.enable()
+    }
+    else {
+        window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+}
+
+async function loadCandidates(id) {
+
+    var allCandidates = [];
+    const candCount = await election.methods.candidatesCount().call();
+    console.log(candCount)
+    for (var i = 1; i <= candCount; i++) {
+        var candidates = await election.methods.candidates(i).call();
+
+        if (candidates.election_id === id) {
+
+            //    return candidates;
+            allCandidates.push(candidates);
+
         }
     }
-
-    async function loadCandidates(id) {
-      
-        var allCandidates=[];
-            const candCount = await election.methods.candidatesCount().call();
-             console.log(candCount)
-            for (var i = 1; i <= candCount; i++) {
-                var candidates = await election.methods.candidates(i).call();
-              
-                if(candidates.election_id === id){
-                   
-                //    return candidates;
-                    allCandidates.push( candidates);
-                   
-                }
-            }
-            return allCandidates;    
+    return allCandidates;
     //   return {};
-    }
+}
 
- 
 
-    async function addCand(candidateId,candidateName,candidateDetails,id,imageUrl) {
-        
-       console.log(account);
-        election.methods.addCandidate(candidateId,candidateName,candidateDetails,id.toString(),imageUrl).send({from:account ,gas:520000  })
+
+async function addCand(candidateId, candidateName, candidateDetails, id, imageUrl) {
+
+    console.log(account);
+    election.methods.addCandidate(candidateId, candidateName, candidateDetails, id.toString(), imageUrl).send({ from: account, gas: 520000 })
         .once('receipt', (receipt) => {
             console.log(receipt);
-          return receipt;
+            return receipt;
         })
-    }
+}
 
 
 
-   async function vote(id) {
-        
-       console.log(account);
+async function vote(id) {
 
-        election.methods.vote(id).send({ from: account ,gas:520000})
+    console.log(account);
+
+    election.methods.vote(id).send({ from: account, gas: 520000 })
         .once('receipt', (receipt) => {
-       console.log(receipt);
-             return receipt;
+            console.log(receipt);
+            return receipt;
         })
-    }
+}
 
-   
-    
-    
- 
+
+
+
+
 
 // // abi = JSON.parse('[{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]');
 
@@ -133,7 +133,7 @@ async function loadBlockchainData(){
 //       console.log(contractInstance.totalVotesFor.call(candidateName).toString());
 //       window.location = "/voteadded/" + id;
 //     });
-  
+
 // }
 
 
